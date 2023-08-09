862e75b3cb6399205d8dea4e2ed11fcf7f1029f7
Andres Perdomo <santiperdomo20@gmail.com>
2023-08-09 17:39:41 -0300

    se agregó un saludo al clickear en el div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var divElement = document.getElementById("miDiv");
+
+    divElement.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
